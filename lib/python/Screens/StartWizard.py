8857e768abd45fc0e4c6fff7cd90df5d8bde5ab4
Boris <boris.fox.solovyev@gmail.com>
2022-12-24 12:59:39 +0200

    Update StartWizard.py

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -3,7 +3,7 @@ from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
 from Screens.WizardLanguage import WizardLanguage
 from Screens.Time import TimeWizard
-from Screens.Rc import Rc
+from Screens.HelpMenu import ShowRemoteControl
 from Tools.HardwareInfo import HardwareInfo
 try:
 	from Plugins.SystemPlugins.OSDPositionSetup.overscanwizard import OverscanWizard
@@ -25,11 +25,11 @@ config.misc.languageselected = ConfigBoolean(default=True)
 config.misc.do_overscanwizard = ConfigBoolean(default=OverscanWizard and config.skin.primary_skin.value == "PLi-FullNightHD/skin.xml")
 
 
-class StartWizard(WizardLanguage, Rc):
+class StartWizard(WizardLanguage, ShowRemoteControl):
 	def __init__(self, session, silent=True, showSteps=False, neededTag=None):
 		self.xmlfile = ["startwizard.xml"]
 		WizardLanguage.__init__(self, session, showSteps=False)
-		Rc.__init__(self)
+		ShowRemoteControl.__init__(self)
 		self["wizard"] = Pixmap()
 
 	def markDone(self):
